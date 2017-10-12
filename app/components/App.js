248c228eedb66722d1e46f6d35e12389c9a0e21a
Karen Zapata <kzapata0919@gmail.com>
2017-10-11 20:19:50 -0700

    Add Home and Battle routes

--- app/components/App.js
+++ app/components/App.js
@@ -4,6 +4,8 @@ var ReactRouter = require('react-router-dom');
 var Router = ReactRouter.BrowserRouter;
 var Route = ReactRouter.Route;
 var Nav = require('./Nav');
+var Home = require('./Home');
+var Battle = require('./Battle');
 //state
 //lifecycle event
 //UI
@@ -21,6 +23,8 @@ class App extends React.Component {
       <Router>
         <div className='container'>
           <Nav />
+          <Route exact path='/' component={Home} />
+          <Route path='/battle' component={Battle} />
           <Route path='/popular' component={Popular} />
         </div>
       </Router>
